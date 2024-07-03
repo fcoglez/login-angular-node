4fdcd5721875f1114c147aeab8c23d8bbb2401c4
Francisco González <franciscogonzalezhontoria@gmail.com>
2024-07-03 14:21:58 +0200

    config sequelize in the file db/connection

---
+++ server/src/db/connection.ts
@@ -0,0 +1,21 @@
+import { Sequelize } from "sequelize";
+
+
+// El código configura una conexión a una base de datos MySQL llamada 
+// logindb usando el usuario root y la contraseña 123456,
+//  con el servidor de la base de datos corriendo en localhost. 
+//  Sequelize es una instancia que puedes usar posteriormente para realizar 
+//  consultas y operaciones sobre la base de datos, como crear, leer, 
+//  actualizar y eliminar registros.
+const sequelize = new Sequelize('logindb', 'root', '123456', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+
+
+
+
+
+
+export default sequelize;
